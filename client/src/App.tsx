873f07ff6042f2d0f054abb2ae5c8eba77b3efd8
Oliver Fei <ofei006@uottawa.ca>
2024-05-04 15:21:44 -0400

    f

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -411,10 +411,13 @@ I will be afflicted that I do not know men."
   let height = 256;
   const Ground = () => {
     const [groundPosition, setGroundPosition] = useState({ x: 0, y: 0 });
-    const speedPixelsPerMillisecond = 50 / 1000; // 50 pixels per second
+    const speedPixelsPerMillisecond = 100 / 1000; // 100 pixels per second
     useTick((delta) => {
       setGroundPosition((prev) => {
-        return { x: (prev.x - delta * speedPixelsPerMillisecond) % 32, y: 0 };
+        return {
+          x: (prev.x - delta * speedPixelsPerMillisecond) % width,
+          y: 0,
+        };
       });
     });
     return (
